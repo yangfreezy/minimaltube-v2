c8ff8f6fe99babf3055ad6e28f2d6fc87ca937ab
yang <yueyangyu42@gmail.com>
2020-02-13 18:42:49 +0800

    Add thumbnail src guard; Remove console.log;

--- src/Components/VideoThumbnail.js
+++ src/Components/VideoThumbnail.js
@@ -4,12 +4,14 @@ import lodash from "lodash";
 import "../App.css";
 
 const VideoThumbnail = ({ video, setMainYoutubeVideo }) => {
-  console.log(video);
   return (
     <div className="video-thumbnail">
       <img
         alt={lodash.unescape(video.snippet.title)}
-        src={video.snippet.thumbnails.high.url}
+        src={
+          video.snippet.thumbnails.high.url ||
+          video.snippet.thumbnails.medium.url
+        }
         onClick={e => setMainYoutubeVideo(video)}
       />
     </div>
