bf33b6f90e8879b34e66745171b0f3769777c6ac
yang <yueyangyu42@gmail.com>
2020-03-17 23:24:14 -0700

    Use const

--- src/Helpers/formatVideoData.js
+++ src/Helpers/formatVideoData.js
@@ -7,22 +7,16 @@ const truncateString = (str, maxLength) => {
 
 // Formats video data from Youtube and splits them into main and relevant videos
 const formatVideoData = videos => {
-
-  // Format videos
-  let formattedVideos = videos.map(video => {
+  const formattedVideos = videos.map(video => {
+    const { snippet: { title, thumbnails }, id: { videoId } } = video;
 
     // HTML decode and truncate
-    const { snippet: { title } } = video;
+    const decodedTitle = lodash.unescape(title);
+    const capitalizedTitle = decodedTitle[0].toUpperCase() + decodedTitle.slice(1);
+    const mainTitle = truncateString(capitalizedTitle, 200)
+    const thumbnailTitle = truncateString(capitalizedTitle, 45);
 
-    let decodedTitle = lodash.unescape(title);
-    let capitalizedTitle = decodedTitle[0].toUpperCase() + decodedTitle.slice(1);
-
-    let mainTitle = truncateString(capitalizedTitle, 200)
-    let thumbnailTitle = truncateString(capitalizedTitle, 45);
-
-    const { id: { videoId }, snippet: { thumbnails } } = video;
-
-    let highestQualityThumbnail = thumbnails.high.url || thumbnails.medium.url || thumbnails.default.url;
+    const highestQualityThumbnail = thumbnails.high.url || thumbnails.medium.url || thumbnails.default.url;
 
     return {
       title: mainTitle,
@@ -33,7 +27,7 @@ const formatVideoData = videos => {
   });
 
   // Split videos into main and relevant
-  let formattedVideoData = {
+  const formattedVideoData = {
     mainVideo: formattedVideos[0],
     relevantVideos: formattedVideos.slice(1),
     ok: true
